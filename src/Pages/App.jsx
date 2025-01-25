dcb78de503d9357d2ff57a2ead34b9ae30664513
Luizin <Luisboasvilas9@gmail.com>
2025-01-25 09:04:23 -0300

    Reorganização das pastas

---
+++ src/Pages/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className='Container'>
+        <h1>Login</h1>
+        <input type="text" placeholder='Username'/>
+        <input type="password" placeholder='Password'/>
+        <button>Submit</button>
+     </div>
+    </>
+  )
+}
+
+export default App
