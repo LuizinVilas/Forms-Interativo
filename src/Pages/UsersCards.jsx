f78f36efc9be3c07a0e6f3229dbb40f73b50cef9
Luizin <Luisboasvilas9@gmail.com>
2025-01-26 11:01:53 -0300

    Criação das props

---
+++ src/Pages/UsersCards.jsx
@@ -0,0 +1,13 @@
+import './UsersCards.css';
+
+function UserCard(props){
+    return (
+        <div className='Card'>
+            <p>Name: <span>{props.Username}</span> </p>
+            <p>Password: <span>{props.Password}</span> </p>
+            <p>Count: <span>{props.Contas}</span> </p>
+        </div>
+    )
+}
+
+export default UserCard;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './Pages/App.jsx'
+import UserCard from './Pages/UsersCards.jsx'
+
+const Users = [
+  {
+    Id: 1,
+    Username: 'Luis',
+    Password: '474101',
+    Contas: 4000
+  },
+  {
+    Id: 2,
+    Username: 'Maria De Fátima',
+    Password: '474101boas',
+    Contas: 200
+  },
+  {
+    Id: 3,
+    Username: 'Leonardo',
+    Password: '474101Boas',
+    Contas: 2000
+  }
+];
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    {
+      Users.map(User => {
+        return (
+          <UserCard key={User.Id} Username = {User.Username} Password = {User.Password} Contas = {User.Contas}/>
+        )
+      })
+    }
+  </StrictMode>
 )
