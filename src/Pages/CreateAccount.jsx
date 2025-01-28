acede53dd5f3ab59ba24675dc9e8d60f9d63c717
Luizin <Luisboasvilas9@gmail.com>
2025-01-28 14:25:29 -0300

    Criação das requisições

--- src/Pages/CreateAccount.jsx
+++ src/Pages/CreateAccount.jsx
@@ -1,18 +1,36 @@
 import './App.css'
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import App from './App'
 
 function CreateAccount(){
+    const [Username, SetUsername] = useState('');
+    const [Password, SetPassword] = useState('');
+    const [ConfirmPassword, SetConfirmPassword] = useState('');
+
+    async function EnviarRequisicao(event) {
+        event.preventdefault();
+        const DadosForms = {Username: Username, Password: Password, ConfirmPassword: ConfirmPassword}
+        await fetch('127.0.0.1:3000/ProcessarDados', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(DadosForms)
+        })
+        .then(Response => Response.json())
+        .then()
+    }
+
     return (
         <>
-           <div className='Container'>
+           <form className='Container' id='FadeInElement' onSubmit={EnviarRequisicao}>
                 <h1>Create Account</h1>
-                <input type="text" placeholder='Username' required/>
-                <input type="text" placeholder='Password' required/>
-                <input type="password" placeholder='Confirm Password' required/>
+                <input type="text" placeholder='Username' value={Username} onChange={(event) => SetUsername(event.target.value)} required/>
+                <input type="text" placeholder='Password' value={Password} onChange={(event) => SetPassword(event.target.value)} required/>
+                <input type="password" placeholder='Confirm Password' value={ConfirmPassword} onChange={(event) => SetConfirmPassword(event.target.value)} required/>
                 <button>Submit</button>
                 <Link to="/"><a href='#'>Already have a account?</a></Link>
-            </div> 
+            </form> 
         </>
     ) 
 }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 function Login(){
     return (
         <>
-           <div className='Container'>
+           <form className='Container' id='FadeInElement'>
                 <h1>Login</h1>
                 <input type="text" placeholder='Username' required/>
                 <input type="password" placeholder='Password' required/>
                 <button>Submit</button>
                 <Link to="/CreateAccount"><a href='#'> Don't have a account? </a></Link>
-            </div> 
+            </form> 
         </>
     ) 
 }
